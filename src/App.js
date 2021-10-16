a5d11e94b058454c3d133552b9152cd1da738cac
enzorusso <enzorussor1@gmail.com>
2021-10-15 23:53:00 -0300

    Adicionando funções filtro para classificação de criança

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-
-
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
 
 function App() {
   const [pessoas, setPessoas] = useState([]);
+  const [filtroPessoas, setFiltroPessoas] = useState([]);
   const [novoNome, setNovoNome] = useState('');
   const [novaIdade, setNovaIdade] = useState('');
+  const [selectedOption, setSelectedOption] = useState('Todos');
+  const [, updateState] = useState(0);
+  const forceUpdate = useCallback(() => updateState((tick) => tick + 1), []);
 
   useEffect(() => {
     fetch('./pessoas.json', {
@@ -20,6 +26,14 @@ function App() {
       .then(res => setPessoas(res.dadosPessoas))
     },[]);
 
+    useEffect(() => {
+      fetch('./pessoas.json', {
+        headers: {
+          Accept: "application/json"
+        }
+      }).then(res => res.json())
+        .then(res => setFiltroPessoas(res.dadosPessoas))
+      },[]);
 
   function setNovoNomeFunction({target}) {
     setNovoNome(target.value);
@@ -32,48 +46,44 @@ function App() {
   function addPessoa() {
     const listaPessoasCopia = Array.from(pessoas);
     listaPessoasCopia.push({nome: novoNome, idade: novaIdade});
+    setFiltroPessoas(listaPessoasCopia);
     setPessoas(listaPessoasCopia);
-    console.log(listaPessoasCopia);
-  
-    /* Salva o item */
-    // localStorage.setItem("pessoaskey", JSON.stringify(listaPessoasCopia));
-  }
-
-  function atualizaListaPessoas({target}, index) {
-    const listaPessoasCopia = Array.from(pessoas);
-    listaPessoasCopia.splice(index, 1, { id: index, value: target.value });
-    setPessoas(listaPessoasCopia);
+    setSelectedOption('Todos');
+    setNovoNome('');
+    setNovaIdade('');
+    localStorage.setItem("pessoas", JSON.stringify(listaPessoasCopia))
+    forceUpdate();
   }
 
   // FUNÇÕES DE ORDENAÇÃO
   function ordenarCrescenteNome() {
-    pessoas.sort(function(a,b) {
+    filtroPessoas.sort(function(a,b) {
       return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
     });
-    console.log(pessoas);
+    forceUpdate();
   }
 
   function ordenarCrescenteIdade() {
-    pessoas.sort((a, b) => a.idade - b.idade)
-    console.log(pessoas);
+    filtroPessoas.sort((a, b) => a.idade - b.idade)
+    forceUpdate();
   }
 
   function ordenarDecrescenteNome() {
-    pessoas.sort((a, b) => {
+    filtroPessoas.sort((a, b) => {
       if (a.nome > b.nome) return -1;
       if (a.nome < b.nome) return 1;
       return 0;
     });
-    console.log(pessoas);
+    forceUpdate();
   }
 
   function ordenarDecrescenteIdade() {
-    pessoas.sort((a, b) => {
+    filtroPessoas.sort((a, b) => {
       if (a.idade > b.idade) return -1;
       if (a.idade < b.idade) return 1;
       return 0;
     });
-    console.log(pessoas);
+    forceUpdate();
   }
 
   function classificaIdade(idade) {
@@ -88,69 +98,110 @@ function App() {
     }
   }
 
+  function trocaTodos() {
+    setSelectedOption("Todos");
+    setFiltroPessoas(pessoas);
+    forceUpdate();
+  }
+
+  function classificaCrianca(pessoa) {
+    const { idade } = pessoa;
+    console.log(idade);
+    if (idade > 0 && idade <= 12) 
+      return true;
+    return false;
+  }
+  
+  function trocaCrianca() {
+    console.log(filtroPessoas);
+    const filtroCriancas = pessoas.filter(pessoa => 
+      classificaCrianca(pessoa)
+    );
+    setSelectedOption("Criança");
+    setFiltroPessoas(filtroCriancas);
+    console.log(filtroCriancas)
+    console.log(pessoas)
+
+    forceUpdate();
+  }
 
   return (
     <Grid className="grid">
       <Grid>
-        <h1 style={{margin:"10px"}}>
-          Adicionar pessoa  
+        <h1 style={{ margin: "10px" }}>
+          Adicionar pessoa
         </h1>
-        <form style={{margin:"10px"}}>
+        <form style={{ margin: "10px" }}>
           <TextField
             label="Nome"
             id="nome"
             size="small"
-            type="text" 
-            name="nome" 
+            type="text"
+            name="nome"
+            value={novoNome}
             onChange={setNovoNomeFunction}
-            style={{margin:"10px"}}
-          />
-          <br/>
+            style={{ margin: "10px" }} />
+          <br />
           <TextField
             label="Idade"
             id="idade"
             size="small"
-            type="text" name="idade"
+            type="text" 
+            name="idade"
+            value={novaIdade}
             onChange={setNovaIdadeFunction}
-            style={{margin:"10px"}}
-          />
-          <br/>
-          <Button variant="contained" onClick={addPessoa} style={{margin:"10px"}}>
+            style={{ margin: "10px" }} />
+          <br />
+          <Button variant="contained" onClick={addPessoa} style={{ margin: "10px" }}>
             Adicionar
           </Button>
         </form>
+        <br/>
+        <FormLabel style={{ marginLeft: "10px" }} component="legend">Classificação</FormLabel>
+        <RadioGroup
+          aria-label="classificacao"
+          defaultValue="Todos"
+          name="radio-buttons-group"
+        >
+        <FormControlLabel style={{ marginLeft: "10px" }} value="Todos" control={<Radio />} label="Todos" checked={selectedOption === 'Todos'} onClick={trocaTodos} />
+        <FormControlLabel style={{ marginLeft: "10px" }} value="crianca" control={<Radio />} label="Criança" checked={selectedOption === 'Criança'} onClick={trocaCrianca} />
+        <FormControlLabel style={{ marginLeft: "10px" }} value="adolescente" control={<Radio />} label="Adolescente" checked={selectedOption === 'Adolescente'} onClick={() => setSelectedOption('Adolescente')}/>
+        <FormControlLabel style={{ marginLeft: "10px" }} value="adulto" control={<Radio />} label="Adulto" checked={selectedOption === 'Adulto'} onClick={() => setSelectedOption('Adulto')}/>
+        <FormControlLabel style={{ marginLeft: "10px" }} value="idoso" control={<Radio />} label="Idoso" checked={selectedOption === 'Idoso'} onClick={() => setSelectedOption('Idoso')}/>
+        </RadioGroup>
       </Grid>
 
-      <Grid style={{marginLeft:"50px"}}>
-        <h1 style={{margin:"10px"}}>
+      <Grid style={{ marginLeft: "50px" }}>
+        <h1 style={{ margin: "10px" }}>
           Lista de pessoas
         </h1>
-        <Button variant="contained" onClick={ordenarCrescenteNome} style={{margin:"10px"}}>
+        <Button variant="contained" onClick={ordenarCrescenteNome} style={{ margin: "10px" }}>
           Ordenar A-Z
         </Button>
-        <Button variant="contained" onClick={ordenarDecrescenteNome} style={{margin:"10px"}}>
+        <Button variant="contained" onClick={ordenarDecrescenteNome} style={{ margin: "10px" }}>
           Ordenar Z-A
         </Button>
-        <Button variant="contained" onClick={ordenarCrescenteIdade} style={{margin:"10px"}}>
-          Ordenar 0-99
+        <Button variant="contained" onClick={ordenarCrescenteIdade} style={{ margin: "10px" }}>
+          Ordenar Idade Menor-Maior
         </Button>
-        <Button variant="contained" onClick={ordenarDecrescenteIdade} style={{margin:"10px"}}>
-          Ordenar 99-0
+        <Button variant="contained" onClick={ordenarDecrescenteIdade} style={{ margin: "10px" }}>
+          Ordenar Idade Maior-Menor
         </Button>
 
-        {pessoas.map(p => (
-          <h2 style={{margin:"10px"}}>
+        {filtroPessoas.map(p => (
+          <h2 style={{ margin: "10px" }}>
             <strong>Nome: </strong>
             {p.nome}
-            <br/>
+            <br />
             <strong>Idade: </strong>
             {p.idade}
-            <br/>
+            <br />
             <strong>Classificação: </strong>
             {classificaIdade(p.idade)}
           </h2>
         ))}
       </Grid>
+      {localStorage.getItem("pessoas")}
     </Grid>
   );
 }
